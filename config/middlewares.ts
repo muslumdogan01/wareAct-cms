04fd9bb79f851a8303df9a71f6be6e96f6ef7724
muslum <muslumdogan01@gmail.com>
2025-08-10 22:50:21 +0100

    some bugs

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,11 +1,22 @@
-export default [
+module.exports = [
+  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": ["'self'", "data:", "blob:", "wareact-cms.onrender.com"],
+          "media-src": ["'self'", "data:", "blob:", "wareact-cms.onrender.com"],
+          "connect-src": ["'self'", "https://www.wareact.com"],
+        },
+      },
+    },
+  },
+  'strapi::poweredBy',
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
